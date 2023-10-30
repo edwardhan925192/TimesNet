c64b54ed011b2980f321e3d19252b742003be28b
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-10-30 11:21:36 +0900

    Update whole_dataset.py

--- whole_dataset.py
+++ whole_dataset.py
@@ -3,33 +3,27 @@ import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 
 class TimesNetDataset(Dataset):
-    def __init__(self, data_array, configs, train=True):
+    def __init__(self, data_array, configs):
         if isinstance(data_array, pd.DataFrame):
             data_array = data_array.values
         self.data_array = torch.from_numpy(data_array).float()
         self.sequence_length = configs.seq_len
-        self.prediction_length = configs.pred_len
-        self.train = train
+        self.prediction_length = configs.pred_len 
+        self.window_shift = configs.window_shift if hasattr(configs, 'window_shift') else 1       
 
-    def __len__(self):
-        if self.train:
-            return len(self.data_array) - self.sequence_length - self.prediction_length + 1
-        else:
-            return (len(self.data_array)- self.prediction_length) // (self.sequence_length)
+    def __len__(self):                
+        total_shifted_length = len(self.data_array) - self.prediction_length - (self.sequence_length - 1) * self.window_shift        
+        return (total_shifted_length - 1) // self.window_shift + 1
+        
 
 
-    def __getitem__(self, index):
-        if self.train:
-            return (
-                self.data_array[index:index+self.sequence_length],
-                self.data_array[index+self.sequence_length:index+self.sequence_length+self.prediction_length]
-            )
-        else:
-            # For test datasets, each chunk starts at index * (sequence_length + prediction_length) to avoid overlap
-            start_idx = index * (self.sequence_length)
-            input_data = self.data_array[start_idx:start_idx+self.sequence_length]
-            target_data = self.data_array[start_idx+self.sequence_length:start_idx+self.sequence_length+self.prediction_length]
-            return input_data, target_data
+    def __getitem__(self, index):                    
+        start_idx = index * self.window_shift
+        return (
+            self.data_array[start_idx:start_idx+self.sequence_length],
+            self.data_array[start_idx+self.sequence_length:start_idx+self.sequence_length+self.prediction_length]
+        )
+        
 
 # ==================== Anomaly detection ==================== #
 class TimesNetAnomalyDataset(Dataset):
