e03414142095d1e1f84bcea70b0a17195150349e
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-12-12 16:22:07 +0900

    Update whole_dataset.py

--- whole_dataset.py
+++ whole_dataset.py
@@ -44,3 +44,59 @@ class TimeSeriesDataset(Dataset):
         target_sequence = self.data_tensor[end_idx:end_idx + self.prediction_length, self.target_idx]
 
         return input_sequence, target_sequence
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+
+class TimeSeries_ValTestDataset(Dataset):
+    def __init__(self, dataframe, sequence_length, batch_size):
+        """
+        Initialize the dataset with a pandas DataFrame for testing.
+
+        Parameters:
+        dataframe (pd.DataFrame): The input DataFrame.
+        sequence_length (int): The length of the input sequences.
+        batch_size (int): The number of sequences per batch.
+        """
+        self.dataframe = dataframe
+        self.sequence_length = sequence_length
+        self.batch_size = batch_size
+        self.data_tensor = torch.tensor(self.dataframe.values).float()
+
+    def __len__(self):
+        """
+        Return the total number of samples available in the dataset.
+        """
+        # Ensure the length is the same as the batch size
+        return self.batch_size
+
+    def __getitem__(self, index):
+        """
+        Generate one sample of data for testing.
+        """
+        # Calculate the start index for each sequence
+        total_length = len(self.dataframe)
+        start_idx = total_length - self.sequence_length - (self.batch_size - 1) + index
+        end_idx = start_idx + self.sequence_length
+
+        # Input features (all columns)
+        input_sequence = self.data_tensor[start_idx:end_idx, :]
+
+        return input_sequence
+
+def create_test_batches(dataframe, sequence_length, batch_size):
+    """
+    Create DataLoader for testing with specified batch size.
+
+    Parameters:
+    dataframe (pd.DataFrame): The input DataFrame.
+    sequence_length (int): The length of the input sequences.
+    batch_size (int): The number of sequences per batch.
+
+    Returns:
+    DataLoader: A DataLoader for the test dataset.
+    """
+    test_dataset = TimeSeries_ValTestDataset(dataframe, sequence_length, batch_size)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    return test_loader
