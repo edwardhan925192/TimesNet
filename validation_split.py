4ae6ee550295726a98c65edaad1acac74921cc5e
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-12-13 14:23:47 +0900

    Update validation_split.py

--- validation_split.py
+++ validation_split.py
@@ -1,16 +1,17 @@
-import pandas as pd 
 import numpy as np 
+import pandas as pd 
 
-def split_train_validation_timeseries(df, validation_ranges, target_column):
+def split_train_validation_timeseries(df, validation_ranges, target_column, configs):
     '''
     Takes a DataFrame and a list of validation ranges (e.g., [(800, 900), (900, 1000)])
     and returns rows previous to the validation range as training data and the target column
-    of the validation range as validation data.
+    of the validation range as validation data, starting from range_start - configs.pred_len.
 
     Parameters:
     df (DataFrame): The DataFrame to split.
     validation_ranges (list of tuples): The ranges for validation data.
     target_column (str): The name of the target column in the DataFrame.
+    configs: Configuration object that includes pred_len.
 
     Returns:
     tuple: A tuple containing two lists of DataFrames, one for training and one for validation.
@@ -21,14 +22,16 @@ def split_train_validation_timeseries(df, validation_ranges, target_column):
     for val_range in validation_ranges:
         # Ensure the range is valid
         start, end = val_range
-        if start >= end or end > len(df):
-            raise ValueError("Invalid range: {}".format(val_range))
+        adjusted_start = start - configs.pred_len  # Adjust start for validation
+
+        if adjusted_start < 0 or start >= end or end > len(df):
+            raise ValueError("Invalid range with adjusted start: {}-{}".format(adjusted_start, end))
 
         # Split the DataFrame
-        validation_df = df.iloc[start:end][target_column]
-        train_df = df.iloc[:start]
+        validation_df = df.iloc[adjusted_start:end][target_column]
+        train_df = df.iloc[:adjusted_start]
 
         train_dfs.append(train_df)
         validation_dfs.append(validation_df)
 
-    return train_dfs,train_dfs, validation_dfs
+    return train_dfs, validation_dfs
