65c2800be7ec069ff6ff00166860d2982f596fba
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-12-13 14:30:21 +0900

    Update whole_dataset.py

--- whole_dataset.py
+++ whole_dataset.py
@@ -45,46 +45,45 @@ class TimeSeriesDataset(Dataset):
 
         return input_sequence, target_sequence
 
-class TimeSeries_ValTestDataset(Dataset):
-    def __init__(self, dataframe, validation_df, target_column, sequence_length, prediction_length, is_test):
+class TimeSeries_ValDataset(Dataset):
+    def __init__(self, dataframe, sequence_length, prediction_length, target_column, batch_size):
         """
-        Initialize the dataset with a pandas DataFrame for testing and validation.
+        Takes single data frame and return sequence length and targets(single) 
 
         Parameters:
         dataframe (pd.DataFrame): The input DataFrame.
-        validation_df (pd.DataFrame): The validation DataFrame.
-        target_column (str): The name of the target column in the validation DataFrame.
         sequence_length (int): The length of the input sequences.
-        prediction_length (int): The length of the target sequences.
-        is_test (bool): Flag to indicate if the dataset is for testing (True) or validation (False).
+        prediction_length (int): The length of the prediction sequences.
+        target_column (str): The name of the target column to predict.
         """
         self.dataframe = dataframe
-        self.validation_df = validation_df
-        self.target_column = target_column
         self.sequence_length = sequence_length
         self.prediction_length = prediction_length
-        self.is_test = is_test
+        self.target_column = target_column
+
+        # Convert DataFrame to a PyTorch tensor
         self.data_tensor = torch.tensor(self.dataframe.values).float()
-        if not is_test:
-            self.target_tensor = torch.tensor(self.validation_df[self.target_column].values).float()
+
+        # Get the index of the target column
+        self.target_idx = self.dataframe.columns.get_loc(target_column)
 
     def __len__(self):
         """
         Return the total number of samples available in the dataset.
         """
-        return 1
+        return batch_size
 
     def __getitem__(self, index):
         """
-        Generate one sample of data for testing or validation.
+        Generate one sample of data.
         """
-        # For testing and validation, return the last 'sequence_length' observations
-        start_idx = len(self.dataframe) - self.sequence_length
-        input_sequence = self.data_tensor[start_idx:, :]
+        start_idx = index
+        end_idx = start_idx + self.sequence_length
 
-        if self.is_test:
-            return input_sequence
-        else:
-            # For validation, return the target values for the prediction length
-            target_value = self.target_tensor[:self.prediction_length]
-            return input_sequence, target_value
+        # Input features (all columns)
+        input_sequence = self.data_tensor[start_idx:end_idx, :]
+
+        # Target output (only the target column)
+        target_sequence = self.data_tensor[end_idx:end_idx + self.prediction_length, self.target_idx]
+
+        return input_sequence, target_sequence
