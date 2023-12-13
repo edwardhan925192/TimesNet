c0aa74b5329e630239c4dbb71ff56c5f30a966ba
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-12-13 12:57:55 +0900

    Update times_config.py

--- times_config.py
+++ times_config.py
@@ -1,14 +1,13 @@
 class Config:
     def __init__(self):
         # ==================== Task name ================ # 
-        self.task_name = 'anomaly_detection'
-
+        self.task_name = 'short_term_forecast'
         # Output features and c_out should be the same when the task is anomaly_detection
 
         # =================== Datasets Shape================== # 
-        self.seq_len = 24    
+        self.seq_len = 180    
         self.window_shift = 1        
-        self.enc_in = 4    # Features          
+        self.enc_in = 12    # Features          
 
         # ================== MODEL ====================== #  
         self.d_model = 32     # Embedding dimension
@@ -16,12 +15,14 @@ class Config:
         self.d_ff = 32        # Output layer dimension
         self.num_kernels = 6  # inception block에서 사용될 커널숫자 
         self.dropout = 0    
-        self.e_layers = 1     # num Timeblock 
-        self.label_len = 24  # 예측할 sequence                 
+        self.e_layers = 1     # num Timeblock         
+        self.label_len = 12   # Features
+
+        self.target_col = 8   # location of target column 
 
         # ================= Output shape ================= # 
-        self.pred_len = 24  
-        self.c_out = 4        # Output feature
+        self.pred_len = 90  
+        self.c_out = 1        # Output feature
         
 
 
