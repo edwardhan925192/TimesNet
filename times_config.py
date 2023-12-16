c3488f5123db5246b15a67e21c5c705146be0c3f
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-12-16 14:58:27 +0900

    Update times_config.py

--- times_config.py
+++ times_config.py
@@ -1,29 +1,31 @@
+num_features = 3
+target_index = 0
+
 class Config:
     def __init__(self):
-        # ==================== Task name ================ # 
+        # ==================== Task name ================ #
         self.task_name = 'short_term_forecast'
         # Output features and c_out should be the same when the task is anomaly_detection
 
-        # =================== Datasets Shape================== # 
-        self.seq_len = 180    
-        self.window_shift = 1        
-        self.enc_in = 12    # Features          
+        # =================== Datasets Shape================== #
+        self.seq_len = 365
+        self.window_shift = 1
+        self.enc_in = num_features    # Features
 
-        # ================== MODEL ====================== #  
-        self.d_model = 32     # Embedding dimension
-        self.top_k = 5        # FFT frequency
-        self.d_ff = 32        # Output layer dimension
-        self.num_kernels = 6  # inception block에서 사용될 커널숫자 
-        self.dropout = 0    
-        self.e_layers = 1     # num Timeblock         
-        self.label_len = 12   # Features
+        # ================== MODEL ====================== #
+        self.d_model = 20     # Embedding dimension
+        self.top_k = 3        # FFT frequency
+        self.d_ff = 20       # Output layer dimension
+        self.num_kernels = 6  # inception block에서 / If using dcvn set it to 3        
+        self.dropout = 0.1    # Dropout rate
+        self.e_layers = 1     # num Timeblock
+        self.label_len = num_features   # Features
 
-        self.target_col = 8   # location of target column 
+        self.target_col = target_index   # location of target column
+        self.cnn_type = 'inception' # dcvn, inception
 
-        # ================= Output shape ================= # 
-        self.pred_len = 90  
+        # ================= Output shape ================= #
+        self.pred_len = 340   # Prediction length
         self.c_out = 1        # Output feature
-        
-
 
 configs = Config()
