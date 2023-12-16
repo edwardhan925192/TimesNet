9b846232388bb9e0d9942136103d931fd368e99d
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-12-16 14:16:58 +0900

    Update deformableconv2d.py

--- cnn_blocks/deformableconv2d.py
+++ cnn_blocks/deformableconv2d.py
@@ -69,21 +69,18 @@ class DeformableConv2d(nn.Module):
 
 class DeconformableBlock(nn.Module):
     '''
-    Similar to Inception_Block_V1, but uses Deformable Convolution layers.
-    After reshaping 1D data to 2D, it goes over Deformable Convolution layers with different kernel sizes.
-    The results are averaged and returned.
+    Uses Deformable Convolution layers with kernel sizes based on the num_kernels parameter.
+    After reshaping 1D data to 2D, it goes over a Deformable Convolution layer with a specified kernel size.
+    The kernel size is determined by the num_kernels parameter, which represents the size of grid cells.
     '''
-    def __init__(self, in_channels, out_channels, num_kernels=6, init_weight=True):
+    def __init__(self, in_channels, out_channels, num_kernels=3, init_weight=True):
         super(DeconformableBlock, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.num_kernels = num_kernels
-        kernels = []
 
-        # Kernel size increases
-        for i in range(self.num_kernels):
-            kernels.append(DeformableConv2d(in_channels, out_channels, kernel_size=2 * i + 1, padding=i))
-        self.kernels = nn.ModuleList(kernels)
+        # Create a single DeformableConv2d layer with kernel size based on num_kernels
+        self.kernel = DeformableConv2d(in_channels, out_channels, kernel_size=num_kernels, padding=num_kernels // 2)
 
         if init_weight:
             self._initialize_weights()
@@ -91,7 +88,7 @@ class DeconformableBlock(nn.Module):
     def _initialize_weights(self):
         for m in self.modules():
             if isinstance(m, DeformableConv2d):
-                # Initialization for Deformable Convolution might differ from standard Conv2d
+                # Initialization for Deformable Convolution
                 m.regular_conv.apply(self._init_conv)
 
     def _init_conv(self, conv):
@@ -101,10 +98,6 @@ class DeconformableBlock(nn.Module):
                 nn.init.constant_(conv.bias, 0)
 
     def forward(self, x):
-        res_list = []
-        for kernel in self.kernels:
-            res_list.append(kernel(x))
-
-        # Average the outputs of all kernels
-        res = torch.stack(res_list, dim=-1).mean(-1)
+        # Apply the deformable convolution layer
+        res = self.kernel(x)
         return res
