78023e6e79a9820e4f12b67c37fddd1c7be56d23
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-12-16 10:49:28 +0900

    Update times_traintest.py

--- times_traintest.py
+++ times_traintest.py
@@ -9,131 +9,200 @@ from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from tqdm import tqdm
 from timesmodel import Model
-from whole_dataset import TimeSeriesDataset,TimeSeries_ValTestDataset
+from whole_dataset import TimeSeriesDataset,TimeSeries_ValDataset,TimeSeries_TestDataset
 import json
 import pandas as pd
+import copy
 #from times_config import configs
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def train_model(model, df_train, df_validation, df_validation_target, target_col, learning_rate, num_epochs, batch_sizes, configs):
+def train_model(model, output_type, df_train, df_validation, target_col, learning_rate, num_epochs, batch_sizes, configs, criterion):
     '''
-    1. Takes model trainset and validation set 
+    1. Takes model trainset and validation set
     2. Load Data with datasets
-    3. Train (schedular, criterion, optimizer) 
-    4. Predict validation set 
+    3. Train (schedular, criterion, optimizer)
+    4. Predict validation set
     '''
-    
-    criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    scheduler = CosineAnnealingLR(optimizer, T_max=num_epochs)
-    
-    # ========================== DATASETS (Task_type) ========================== # 
-    if configs.task_name == 'short_term_forecast':
-        train_dataset = TimesNetDataset(df_train, configs.seq_len, configs.pred_len, target_col)
-        train_loader = DataLoader(train_dataset, batch_size=batch_sizes, shuffle=False)        
-        
-        val_dataset = TimesNetDataset_valtest(df_validation, df_validation_target,target_col, configs.seq_length, configs.pred_length,  is_test=False)    
-        val_loader = DataLoader(train_dataset, batch_size=batch_sizes, shuffle=False)                        
 
-    # ========================== TRAIN (Task_type) ========================== # 
-    if configs.task_name == 'short_term_forecast':
+    best_val_loss = float('inf')
+    best_model_state = None
+    best_epoch = -1
+
+    model_type = model
+
+    # ==================== MODEL SELECTION ========================== #
+    if model == 'timesnet':
+      model = Model(configs).to(device)
+
+    # ==================== CRITERION ========================== #
+    if criterion =='mse':
+        criterion = nn.MSELoss()
+    if criterion =='mae':
+        criterion = nn.L1Loss()
+    
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=1, eta_min=0.0005)
+
+    # ==================== TRAINING ========================== #
+    if configs.task_name == 'short_term_forecast':        
+        train_dataset = TimeSeriesDataset(output_type, df_train, configs.seq_len, configs.pred_len, target_col)
+        train_loader = DataLoader(train_dataset, batch_size=batch_sizes, shuffle=False)
+
         for epoch in range(num_epochs):
             model.train()
             total_loss = 0
-            for batch_data, batch_target in tqdm(train_loader, desc=f"Epoch {epoch + 1}/{num_epochs}"):
+
+            for batch_idx, (batch_data, batch_target) in enumerate(tqdm(train_loader, desc=f"Epoch {epoch + 1}/{num_epochs}")):
                 batch_data, batch_target = batch_data.to(device), batch_target.to(device)
                 optimizer.zero_grad()
                 outputs = model(batch_data)
+
+                if outputs.shape[-1] <= 1:
+                  outputs = outputs.squeeze(-1)
+
                 loss = criterion(outputs, batch_target)
                 loss.backward()
                 optimizer.step()
                 total_loss += loss.item()
-    
-            #  =========== Validation step ================ # 
-            
-                model.eval()
+                scheduler.step(epoch + batch_idx / len(train_loader))
+
+            average_training_loss = total_loss / len(train_loader)
+
+            # Validation step after each epoch for each validation set
+            model.eval()
+            total_val_loss = 0
+
+            for val_df in df_validation:
+                val_dataset = TimeSeries_ValDataset(output_type, val_df, configs.seq_len, configs.pred_len, target_col, batch_sizes)
+                val_loader = DataLoader(val_dataset, batch_size=batch_sizes, shuffle=False)
+
                 val_loss = 0
-                with torch.no_grad():                                        
+                with torch.no_grad():
                     for batch_data, batch_target in val_loader:
                         batch_data, batch_target = batch_data.to(device), batch_target.to(device)
                         outputs = model(batch_data)
+
+                        if outputs.shape[-1] <= 1:
+                          outputs = outputs.squeeze(-1)
+
                         loss = criterion(outputs, batch_target)
                         val_loss += loss.item()
-    
-                print(f"Epoch {epoch + 1}/{num_epochs}, Training Loss: {total_loss / len(train_loader)}, Validation Loss: {val_loss / len(val_loader)}")
-            else:
-                print(f"Epoch {epoch + 1}/{num_epochs}, Training Loss: {total_loss / len(train_loader)}")
 
-            scheduler.step() 
-            
-    elif configs.task_name == 'anomaly_detection':
-        for epoch in range(num_epochs):
-            model.train()
-            total_loss = 0
-            for batch_data in tqdm(train_loader, desc=f"Epoch {epoch + 1}/{num_epochs}"):
-                batch_data = batch_data.to(device)
+                total_val_loss += val_loss / len(val_loader)
+
+            average_validation_loss = total_val_loss / len(df_validation)
+
+            print(f"Epoch {epoch + 1}/{num_epochs}, Training Loss: {average_training_loss}, Average Validation Loss: {average_validation_loss}")
+
+            # Update best validation loss and epoch
+            if average_validation_loss < best_val_loss:
+                best_val_loss = average_validation_loss
+                best_epoch = epoch
+                print(outputs)
+                best_model_state = copy.deepcopy(model.state_dict())
+
+
+    print(f"Best Epoch: {best_epoch + 1} with Validation Loss: {best_val_loss}")
+
+    return best_epoch, best_model_state
+
+def test_model(model, output_type, test,target_col,learning_rate, num_epochs,batch_sizes, configs, criterion):
+    '''
+    Retrain the model with full datasets and make a final prediction
+    '''
+
+    model_type = model
+    best_model_state = None
+    # ==================== MODEL SELECTION ========================== #
+    if model == 'timesnet':
+      model = Model(configs).to(device)
+
+    # ==================== CRITERION ========================== #
+    if criterion =='mse':
+        criterion = nn.MSELoss()
+    if criterion =='mae':
+        criterion = nn.L1Loss()
+    
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=1, eta_min=0.0005)
+
+    test_dataset = TimeSeries_TestDataset(test, configs.seq_len, batch_sizes)
+    test_loader = DataLoader(test_dataset, batch_size=batch_sizes, shuffle=False)
+    
+    train_dataset = TimeSeriesDataset(output_type, test, configs.seq_len, configs.pred_len, target_col)
+    train_loader = DataLoader(train_dataset, batch_size=batch_sizes, shuffle=False)
+
+    for epoch in range(num_epochs):
+        model.train()
+
+        for batch_idx, (batch_data, batch_target) in enumerate(tqdm(train_loader, desc=f"Epoch {epoch + 1}/{num_epochs}")):
+                batch_data, batch_target = batch_data.to(device), batch_target.to(device)
+
                 optimizer.zero_grad()
                 outputs = model(batch_data)
-                loss = criterion(outputs, batch_data)
+
+                if outputs.shape[-1] <= 1:
+                  outputs = outputs.squeeze(-1)
+
+                loss = criterion(outputs, batch_target)
                 loss.backward()
                 optimizer.step()
-                total_loss += loss.item()
-                
-        scheduler.step()
-    # =========================================================================== #
+                scheduler.step(epoch + batch_idx / len(train_loader))
 
-def test_model(model, test,batch_sizes, configs):
-    criterion = nn.MSELoss()
-    test_dataset = TimesNetDataset(np.array(test), configs, train=False)
+
+    best_model_state = copy.deepcopy(model.state_dict())
+    model.eval()
+    predictions = []
+    with torch.no_grad():
+        for batch_test_data in test_loader:
+            batch_test_data = batch_test_data.to(device)
+            print(batch_test_data)
+            outputs = model(batch_test_data)
+            predictions.extend(outputs.cpu().numpy())
+
+    return predictions,best_model_state
+
+# ====================== Train, Test MAIN ========================= #
+def timesnetmain(model,output_type,df_train, df_validation, df_test, target_col, learning_rate, num_epochs, batch_sizes, configs, criterion):
+    if model == 'timesnet':
+      model = Model(configs).to(device)
+
+    train_data = train
+    # ===== train and validate model ===== #
+    best_epoch, train_model_state = train_model(model,output_type, df_train, df_validation,  target_col, learning_rate, num_epochs, batch_sizes, configs, criterion)
+
+    # from validation get best epoch and retrain with full datasets and return the prediction of last one
+    best_epoch = best_epoch + 1
+    full_training_set = df_test
+
+    # ===== test model ===== #
+    pred, test_model_state = test_model(model, output_type, df_test,target_col, learning_rate, best_epoch, batch_sizes,configs, criterion)
+    return pred,train_model_state, test_model_state
+
+def test_model_with_weights(model_type, state_dict, test,  batch_sizes, configs, criterion):
+    '''
+    Retrain the model with full datasets and make a final prediction
+    '''
+    model = Model(configs).to(device)
+     # Load the provided model state dictionary
+    model.load_state_dict(state_dict)
+
+    model.eval()  # Set the model to evaluation mode
+
+    # Prepare the test data
+    test_dataset = TimeSeries_TestDataset(test, configs.seq_len, batch_sizes, criterion)
     test_loader = DataLoader(test_dataset, batch_size=batch_sizes, shuffle=False)
 
-    model.eval()
-    test_loss = 0
+    # Make predictions
     predictions = []
-    true_values = []
     with torch.no_grad():
-        for batch_data, batch_target in test_loader:
-            batch_data, batch_target = batch_data.to(device), batch_target.to(device)
-            outputs = model(batch_data)
-            loss = criterion(outputs, batch_target)
-            test_loss += loss.item()            
-            
+        for batch_test_data in test_loader:
+            batch_test_data = batch_test_data.to(device)
+            outputs = model(batch_test_data)
             predictions.extend(outputs.cpu().numpy())
-            true_values.extend(batch_target.cpu().numpy())
 
-    print(f"Testing Loss: {test_loss / len(test_loader)}")
-    return predictions, true_values
-
-def save_results(predictions, true_values, filename='results.csv'):
-    # Convert predictions and true_values to DataFrame
-    pred_df = pd.concat([pd.DataFrame(item) for item in predictions], ignore_index=True)
-    true_df = pd.concat([pd.DataFrame(item) for item in true_values], ignore_index=True)
-
-    combined_df = pd.concat([pred_df, true_df], axis=1)
-    combined_df.columns = [f"Pred_{i}" for i in range(pred_df.shape[1])] + [f"True_{i}" for i in range(true_df.shape[1])]
-
-    combined_df.to_csv(filename, index=False)
-
-def load_data_from_path(filepath):
-    """Load data from the given path into a pandas dataframe."""
-    return pd.read_csv(filepath)
-    
-# ================================================================= #
-# ====================== Train, Test MAIN ========================= #
-# ================================================================= #
-def timesnetmain(Model,train_df, validation_train_df, validation_target_df, configs):
-    model = Model(configs).to(device)
-    train_data = train
-    train_model(model, train_data, configs.lr, configs.epochs, configs.batch_sizes, configs, val_data)
-
-# Test the model on each test dataset
-all_predicted_values = []
-all_ground_truth_values = []
-
-predicted_values, ground_truth_values = test_model(model, test_data, configs.batch_sizes, configs)
-
-all_predicted_values.extend(predicted_values)
+    return predictions
 all_ground_truth_values.extend(ground_truth_values)
     
 save_results(all_predicted_values, all_ground_truth_values)
