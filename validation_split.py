38a337e8060ca179644458a475258b3077063001
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-12-16 11:08:05 +0900

    Update validation_split.py

--- validation_split.py
+++ validation_split.py
@@ -1,21 +1,22 @@
 import numpy as np 
 import pandas as pd 
 
-def split_train_validation_timeseries(df, validation_ranges, target_column, seq_len):
+def split_train_validation_timeseries(df, validation_ranges, target_column, seq_len, single=False):
     '''
     Takes a DataFrame and a list of validation ranges (e.g., [(800, 900), (900, 1000)])
-    Suppose sequence length is 100 then it returns 700 - 1000 as validation set so that the first prediction will be inside validation set 
-    0 - 800 as training set for the first set 
-    
+    Validation is going to be the Range that is takes MINUS the sequence length e.g, (700,900) 
+    if Seq_length is 100 and the range is from (800, 900)
 
     Parameters:
     df (DataFrame): The DataFrame to split.
     validation_ranges (list of tuples): The ranges for validation data.
     target_column (str): The name of the target column in the DataFrame.
-    configs: Configuration object that includes pred_len.
+    seq_len (int): The sequence length.
+    single (bool): Whether to return only the target column or the entire DataFrame.
 
     Returns:
-    tuple: A tuple containing two lists of DataFrames, one for training and one for validation.
+    tuple: A tuple containing two lists of DataFrames or Series, one for training and one for validation.
+    
     '''
     train_dfs = []
     validation_dfs = []
@@ -29,11 +30,15 @@ def split_train_validation_timeseries(df, validation_ranges, target_column, seq_
             raise ValueError("Invalid range with adjusted start: {}-{}".format(adjusted_start, end))
 
         # Split the DataFrame
-        validation_df = df.iloc[adjusted_start:end]  # Return the entire DataFrame for the range
+        validation_df = df.iloc[adjusted_start:end]
         train_df = df.iloc[:start]
+
+        # Extract only the target column if single is True
+        if single:
+            validation_df = validation_df[target_column]
+            train_df = train_df[target_column]
 
         train_dfs.append(train_df)
         validation_dfs.append(validation_df)
 
     return train_dfs, validation_dfs
-
