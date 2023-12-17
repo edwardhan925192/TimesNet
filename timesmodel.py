1817eb642b2749a8cb8288f884970f823cd88155
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-12-17 09:57:18 +0900

    Update timesmodel.py

--- timesmodel.py
+++ timesmodel.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from torch.nn.utils import weight_norm
 import math
 from cnn_blocks.deformableconv2d import DeformableConv2d,DeconformableBlock
+from cnn_blocks.inception_blocks import Inception_Block_V1,Inception_Block_V2
 
 #Positional embedding used in transformer
 class PositionalEmbedding(nn.Module):
@@ -82,73 +83,6 @@ class DataEmbedding(nn.Module):
       # if x_mark is None:
         x = self.value_embedding(x) + self.position_embedding(x)
         return self.dropout(x)
-
-#Positional embedding is broad casted when used in torch
-class Inception_Block_V1(nn.Module):
-    '''
-    1. After reshaping 1D data to 2D with FFT it goes over convolution layers
-    2. It goes through different kernel_size appended in the module lists
-    3. Averaged and returned
-    '''
-    def __init__(self, in_channels, out_channels, num_kernels=6, init_weight=True):
-        super(Inception_Block_V1, self).__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.num_kernels = num_kernels
-        kernels = []
-
-        # =========== Kernel size increases ========= #
-        for i in range(self.num_kernels):
-            kernels.append(nn.Conv2d(in_channels, out_channels, kernel_size=2 * i + 1, padding=i))
-        self.kernels = nn.ModuleList(kernels)
-        if init_weight:
-            self._initialize_weights()
-
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-
-    def forward(self, x):
-        res_list = []
-        for i in range(self.num_kernels):
-            res_list.append(self.kernels[i](x))
-
-        # ============ Average them ============ #
-        res = torch.stack(res_list, dim=-1).mean(-1)
-        return res
-
-class Inception_Block_V2(nn.Module):
-    def __init__(self, in_channels, out_channels, num_kernels=6, init_weight=True):
-        super(Inception_Block_V2, self).__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.num_kernels = num_kernels
-        kernels = []
-
-        for i in range(self.num_kernels // 2):
-            kernels.append(nn.Conv2d(in_channels, out_channels, kernel_size=[1, 2 * i + 3], padding=[0, i + 1]))
-            kernels.append(nn.Conv2d(in_channels, out_channels, kernel_size=[2 * i + 3, 1], padding=[i + 1, 0]))
-        kernels.append(nn.Conv2d(in_channels, out_channels, kernel_size=1))
-        self.kernels = nn.ModuleList(kernels)
-        if init_weight:
-            self._initialize_weights()
-
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-
-    def forward(self, x):
-        res_list = []
-        for i in range(self.num_kernels + 1):
-            res_list.append(self.kernels[i](x))
-        res = torch.stack(res_list, dim=-1).mean(-1)
-        return res
 
 def FFT_for_Period(x, k=2):
     '''
