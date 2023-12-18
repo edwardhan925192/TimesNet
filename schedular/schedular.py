5327ef6577698c53cbfc6f4c87ac0463617b6b11
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-12-18 10:05:51 +0900

    Update schedular.py

--- schedular/schedular.py
+++ schedular/schedular.py
@@ -18,15 +18,4 @@ def initialize_scheduler(optimizer, configs):
         raise ValueError("Unsupported scheduler type")
 
     return scheduler
-
-class SchedulerConfig:
-    def __init__(self):
-        self.CosineAnnealingWarmRestarts = {'T_0': 10, 'T_mult': 1, 'eta_min': 0.0005}
-        self.StepLR = {'step_size': 10, 'gamma': 0.1}
-        self.ExponentialLR = {'gamma': 0.95}
-        self.OneCycleLR = {'max_lr': 0.01, 'steps_per_epoch': 10, 'epochs': 20}
-        self.CyclicLR = {'base_lr': 0.001, 'max_lr': 0.01, 'step_size_up': 5, 'mode': 'triangular'}
-        # Additional schedulers can be added here
-
-    def get_params(self, scheduler_name):
-        return getattr(self, scheduler_name, None)
+    
