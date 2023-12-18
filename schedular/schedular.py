968ab518c621ad380c36381f35e8d98ded371ae0
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-12-18 10:52:09 +0900

    Update schedular.py

--- schedular/schedular.py
+++ schedular/schedular.py
@@ -2,17 +2,17 @@ import torch
 
 def initialize_scheduler(optimizer, configs):
 
-    scheduler_params = configs.scheduler_config.get_params(configs.scheduler_name)  # Get scheduler parameters
+    scheduler_params = configs.schedular_config.get_params(configs.schedular_name)  # Get scheduler parameters
 
-    if configs.scheduler_name == 'CosineAnnealingWarmRestarts':
+    if configs.schedular_name == 'CosineAnnealingWarmRestarts':
         scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, **scheduler_params)
-    elif configs.scheduler_name == 'StepLR':
+    elif configs.schedular_name == 'StepLR':
         scheduler = torch.optim.lr_scheduler.StepLR(optimizer, **scheduler_params)
-    elif configs.scheduler_name == 'ExponentialLR':
+    elif configs.schedular_name == 'ExponentialLR':
         scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, **scheduler_params)
-    elif configs.scheduler_name == 'OneCycleLR':
+    elif configs.schedular_name == 'OneCycleLR':
         scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, **scheduler_params)
-    elif configs.scheduler_name == 'CyclicLR':
+    elif configs.schedular_name == 'CyclicLR':
         scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, **scheduler_params)
     else:
         raise ValueError("Unsupported scheduler type")
