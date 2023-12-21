072c4195d34f442600648dbc715538f4165c922a
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-12-21 16:03:52 +0900

    Update times_traintest.py

--- times_traintest.py
+++ times_traintest.py
@@ -19,11 +19,34 @@ import matplotlib.pyplot as plt
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def train_model(model, output_type, df_train, df_validation, target_col, learning_rate, num_epochs, batch_sizes, configs, criterion, scheduler_bool):
+import pickle
+import os
+import sys
+import argparse
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm
+from times_model import Model
+from whole_dataset import TimeSeriesDataset,TimeSeries_ValDataset,TimeSeries_TestDataset
+from schedular.scheduler import initialize_scheduler
+import json
+import pandas as pd
+import copy
+import torchvision.ops
+#from times_config import configs
+import matplotlib.pyplot as plt
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+def train_model(model, df_train, df_validation, target_col, learning_rate, num_epochs, batch_sizes, configs, criterion, schedular_bool):
     '''
+    BOTH TRAINING SET AND VALIDATION SET HAVE TO BE LISTS 
+
     1. Takes model trainset and validation set
     2. Load Data with datasets
-    3. Train (scheduler, criterion, optimizer)
+    3. Train (schedular, criterion, optimizer)
     4. Predict validation set
     '''
 
@@ -37,12 +60,14 @@ def train_model(model, output_type, df_train, df_validation, target_col, learnin
     validation_loss_history = []
 
     # ==================== TARGET INDEX ========================== #
-    col_list = list(df_train.columns)
+    col_list = list(df_train[0].columns)
     target_index = col_list.index(target_col) if target_col in col_list else -1
 
     # ==================== MODEL SELECTION ========================== #
     if model == 'timesnet':
       model = Model(configs).to(device)
+    if model == 'itransformer':
+      model = I_T_Model(configs).to(device)
 
     # ==================== CRITERION ========================== #
     if criterion =='mse':
@@ -51,91 +76,103 @@ def train_model(model, output_type, df_train, df_validation, target_col, learnin
         criterion = nn.L1Loss()
 
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    
-    # =================== Scheduler initialization ======================= # 
-    if scheduler_bool:      
+
+    # =================== Schedular initialization ======================= #
+    if schedular_bool:
       scheduler = initialize_scheduler(optimizer, configs)
 
     # ==================== TRAINING ========================== #
-    if configs.task_name == 'short_term_forecast':
-        train_dataset = TimeSeriesDataset(output_type, df_train, configs.seq_len, configs.pred_len, target_col)
-        train_loader = DataLoader(train_dataset, batch_size=batch_sizes, shuffle=False)
+    whole_val_storage = []
 
-        for epoch in range(num_epochs):
-            model.train()
-            total_loss = 0
+    # ==================== INDIVIDUAL TRAINING SETS ====================== # 
+    for train_ , val_ in zip(df_train, df_validation):                
+      if configs.task_name == 'short_term_forecast':
+          train_dataset = TimeSeriesDataset(train_, configs.seq_len, configs.pred_len)
+          train_loader = DataLoader(train_dataset, batch_size=batch_sizes, shuffle=False)
 
-            for batch_idx, (batch_data, batch_target) in enumerate(tqdm(train_loader, desc=f"Epoch {epoch + 1}/{num_epochs}")):
-                batch_data, batch_target = batch_data.to(device), batch_target.to(device)
-                optimizer.zero_grad()
-                outputs = model(batch_data)
+      for epoch in range(num_epochs):
+          val_storage = []
 
-                # ============== OUTPUT TYPE =============== #
-                if output_type == 'single':
-                  outputs = outputs[:, :, target_index]
+          model.train()
+          total_loss = 0
 
-                if outputs.shape[-1] <= 1:
-                  outputs = outputs.squeeze(-1)
+          # ================== INDIVIDUAL EPOCH ===================== #
+          for batch_idx, (batch_data, batch_target) in enumerate(tqdm(train_loader, desc=f"Epoch {epoch + 1}/{num_epochs}")):
+              batch_data, batch_target = batch_data.to(device), batch_target.to(device)
+              optimizer.zero_grad()
+              outputs = model(batch_data)
 
-                loss = criterion(outputs, batch_target)
-                loss.backward()
-                optimizer.step()
-                total_loss += loss.item()
+              # ============== OUTPUT ADJUSTMENT =============== #                  
+              if target_col:
+                  outputs = outputs[:,:, target_col]
+                  batch_target = batch_target[:,:, target_col]
+              
+              # ============== LOSSES ================ #
+              loss = criterion(outputs, batch_target)
+              loss.backward()
+              optimizer.step()
+              total_loss += loss.item()
 
-                # ========== Scheduler ============= #
-                if scheduler_bool and configs.scheduler_update_type == 'batch':
-                  scheduler.step(epoch + batch_idx / len(train_loader))
+              # ========== Schedular ============= #
+              if schedular_bool and configs.scheduler_update_type == 'batch':
+                scheduler.step(epoch + batch_idx / len(train_loader))
 
-            # Update Scheduler after each epoch if specified
-            if scheduler_bool and configs.scheduler_update_type == 'epoch':
-                scheduler.step()
+          # Update Scheduler after each epoch if specified
+          if schedular_bool and configs.scheduler_update_type == 'epoch':
+              scheduler.step()
 
-            average_training_loss = total_loss / len(train_loader)
+          average_training_loss = total_loss / len(train_loader)
 
-            # Validation step after each epoch for each validation set
-            model.eval()
-            total_val_loss = 0
+          # =================== Validation ====================== #               
+          model.eval()
+          total_val_loss = 0
 
-            for val_df in df_validation:
-                val_dataset = TimeSeries_ValDataset(output_type, val_df, configs.seq_len, configs.pred_len, target_col, batch_sizes)
-                val_loader = DataLoader(val_dataset, batch_size=batch_sizes, shuffle=False)
+          for val_df in df_validation:
+              val_dataset = TimeSeries_ValDataset(val_, configs.seq_len, configs.pred_len)
+              val_loader = DataLoader(val_dataset, batch_size=batch_sizes, shuffle=False)
 
-                val_loss = 0
-                with torch.no_grad():
-                    for batch_data, batch_target in val_loader:
-                        batch_data, batch_target = batch_data.to(device), batch_target.to(device)
-                        outputs = model(batch_data)
+              val_loss = 0
 
-                        # ============== OUTPUT TYPE =============== #
-                        if output_type == 'single':
-                          outputs = outputs[:, :, target_index]
+              with torch.no_grad():
+                  for batch_data, batch_target in val_loader:
+                      batch_data, batch_target = batch_data.to(device), batch_target.to(device)
+                      outputs = model(batch_data)
+                      
+                      # ============== OUTPUT ADJUSTMENT =============== #                  
+                      if target_col:
+                        outputs = outputs[:,:, target_col]
+                        batch_target = batch_target[:,:, target_col]
 
-                        if outputs.shape[-1] <= 1:
-                          outputs = outputs.squeeze(-1)
+                      # ============== LOSSES =============== # 
+                      loss_ = criterion(outputs, batch_target)
 
-                        # ========= OUTPUT ADJUSTED ======= #
-                        outputs_adjusted = outputs[:, :batch_target.size(1)]
-                        loss = criterion(outputs_adjusted, batch_target)
-                        # ================================= #
-                        val_loss += loss.item()
+                      val_loss += loss_.item()
 
-                total_val_loss += val_loss / len(val_loader)
+              total_val_loss += val_loss / len(val_loader)
 
-            average_validation_loss = total_val_loss / len(df_validation)
-            training_loss_history.append(average_training_loss)
-            validation_loss_history.append(average_validation_loss)
-            print(f"Epoch {epoch + 1}/{num_epochs}, Training Loss: {average_training_loss}, Average Validation Loss: {average_validation_loss}")
+          average_validation_loss = total_val_loss / len(df_validation)
+          training_loss_history.append(average_training_loss)
+          validation_loss_history.append(average_validation_loss)
+          print(f"Epoch {epoch + 1}/{num_epochs}, Training Loss: {average_training_loss}, Average Validation Loss: {average_validation_loss}")
 
-            # Update best validation loss and epoch
-            if average_validation_loss < best_val_loss:
-                best_val_loss = average_validation_loss
-                best_epoch = epoch
-                print('=================Current_BEST======================')
-                best_model_state = copy.deepcopy(model.state_dict())
+          val_storage.append(average_validation_loss)              
 
-    print(f"Best Epoch: {best_epoch + 1} with Validation Loss: {best_val_loss}")
+          # Update best validation loss and epoch
+          if average_validation_loss < best_val_loss:
+              best_val_loss = average_validation_loss
+              best_epoch = epoch
+              print('=================Current_BEST======================')
+              best_model_state = copy.deepcopy(model.state_dict())
 
-    return training_loss_history, validation_loss_history, best_epoch, best_model_state
+      whole_val_storage.append(val_storage)
+    
+    
+    mean_validation_loss_per_epoch = [sum(epoch_losses) / len(epoch_losses) for epoch_losses in zip(*whole_val_storage)]
+
+    for i in range(len(mean_validation_loss_per_epoch)):
+      print(f"Final BEST Epoch {i + 1}/{num_epochs}, Final Average Validation Loss: {mean_validation_loss_per_epoch[i]}")    
+
+    return training_loss_history, validation_loss_history, mean_validation_loss_per_epoch, best_model_state    
 
 def test_model(model, output_type, test, target_col,learning_rate, num_epochs,batch_sizes, configs, criterion, scheduler_bool):
     '''
